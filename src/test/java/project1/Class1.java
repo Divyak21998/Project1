4816c9e255ebff283864f549b848ab790fec5629
Divyak21998 <divyak21998@email.com>
2023-11-29 13:49:41 +0530

    1st commit

---
+++ src/test/java/project1/Class1.java
@@ -0,0 +1,15 @@
+package project1;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		System.out.println("----main1-----");
+		System.out.println("----main2-----");
+		System.out.println("----main3-----");
+		System.out.println("----main4-----");
+		System.out.println("----main5-----");
+	
+
+	}
+
+}
