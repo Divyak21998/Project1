07399b0dfaa96e8e830b6402c6eb6dd1c2d3c8af
Divyak21998 <divyak21998@email.com>
2023-11-30 10:32:14 +0530

    9th commit

---
+++ src/test/java/project1/Class4_test.java
@@ -0,0 +1,28 @@
+package project1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Class4_test {
+	WebDriver driver;
+	@Test
+	public  void test() {
+		
+		 String BROWSER= System.getProperty("browser");
+		 String URL= System.getProperty("url");
+		 String USERNAME = System.getProperty("username");
+      	 String PASSWORD= System.getProperty("password");
+		 
+		 if(BROWSER.equals("browser")) {
+		  driver=new ChromeDriver();
+		 }
+			driver.manage().window().maximize();
+			driver.get(URL);
+			driver.findElement(By.name("user_name")).sendKeys(USERNAME);
+			driver.findElement(By.name("user_password")).sendKeys(PASSWORD);
+		  
+	}
+
+}
